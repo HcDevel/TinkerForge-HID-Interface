55b1a0cc0401d25beec8bb3ec25dcbac9738e3f8
HcDevel <HcDevel@users.noreply.github.com>
2014-03-08 17:07:10 +0100

    Bug behoben: Joystick wurde nicht angezeigt
    Es muss mindestens ein Button registriert werden um den Joystick anzuzeigen

--- rotary-poti-example.py
+++ rotary-poti-example.py
@@ -4,13 +4,18 @@ from tinkerforge.ip_connection import IPConnection
 from tinkerforge.bricklet_rotary_poti import RotaryPoti
 
 class emulate:
+	def poti_cb(self,position):
+		print(str(position))
+		self.device.emit(uinput.ABS_Z, position) #Setzt den Wert des Joysticks auf den Wert des Potis
+
 	def __init__(self):
 		#uinput Bereich
 		self.events = (
+			uinput.BTN_A, #Es wird mindestens ein Button benötigt (seltsam)
 			uinput.ABS_Z + (-150, 150, 0, 0), #Erstellt Joystick Achse Z, kleinster Wert des Potis ist -150, größter Wert ist +150
 			)
 
-		self.device = uinput.Device(self.events, "TF Virtual HID Joystick")
+		self.device = uinput.Device(self.events, "TF Virutal HID Joystick")
 		
 		#TinkerForge Bereich
 		ipcon = IPConnection()
@@ -19,11 +24,7 @@ class emulate:
 		ipcon.connect("127.0.0.1", 4223) #IP / Port anpassen
 		self.poti.set_position_callback_period(50)
 		self.poti.register_callback(self.poti.CALLBACK_POSITION, self.poti_cb) #Sobald der Callback auslöst wird die Funktion poti_cb aufgerufen
-		
-	def poti_cb(self,position):
-		print(str(position))
-		self.device.emit(uinput.ABS_Z, position) #Setzt den Wert des Joysticks auf den Wert des Potis
 
 if __name__ == "__main__":
 	joy = emulate()
-	input ("Press key to exit...")
+	input ("Press key to exit...\n")
