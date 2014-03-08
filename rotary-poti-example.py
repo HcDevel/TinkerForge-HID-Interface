e870d3832e868ca8624a971fbe9d40f8f5235f2b
HcDevel <hc.devel@yahoo.de>
2014-03-08 16:48:27 +0100

    Rotary Poti Beispiel Hinzugefügt

---
+++ rotary-poti-example.py
@@ -0,0 +1,29 @@
+import uinput
+
+from tinkerforge.ip_connection import IPConnection
+from tinkerforge.bricklet_rotary_poti import RotaryPoti
+
+class emulate:
+	def __init__(self):
+		#uinput Bereich
+		self.events = (
+			uinput.ABS_Z + (-150, 150, 0, 0), #Erstellt Joystick Achse Z, kleinster Wert des Potis ist -150, größter Wert ist +150
+			)
+
+		self.device = uinput.Device(self.events, "TF Virtual HID Joystick")
+		
+		#TinkerForge Bereich
+		ipcon = IPConnection()
+		self.poti = RotaryPoti("aBQ", ipcon) #UID ändern!
+
+		ipcon.connect("127.0.0.1", 4223) #IP / Port anpassen
+		self.poti.set_position_callback_period(50)
+		self.poti.register_callback(self.poti.CALLBACK_POSITION, self.poti_cb) #Sobald der Callback auslöst wird die Funktion poti_cb aufgerufen
+		
+	def poti_cb(self,position):
+		print(str(position))
+		self.device.emit(uinput.ABS_Z, position) #Setzt den Wert des Joysticks auf den Wert des Potis
+
+if __name__ == "__main__":
+	joy = emulate()
+	input ("Press key to exit...")
